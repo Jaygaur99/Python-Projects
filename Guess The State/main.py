ce44102b6b5d6b55eb899bdd844ba42d3571f7e5
Jay Kumar Gaur <jaygaur99@gmail.com>
2021-04-09 09:48:35 +0530

    Nothing

--- Guess The State/main.py
+++ Guess The State/main.py
@@ -21,10 +21,7 @@ while len(guessed_states) < len(data.states):
     answer_state = answer_state.title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv('states_to_learn.csv')
         break
